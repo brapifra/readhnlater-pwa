170b94abc23d1775c239cf8c077bbacb90edb252
Brais Piñeiro <brapifra@gmail.com>
2018-09-02 09:50:32 +0100

    App test

--- src/containers/App/App.test.tsx
+++ src/containers/App/App.test.tsx
@@ -1,9 +1,30 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 import App from '.';
+import Store from '../../redux';
+import Header from '../../components/Header';
+import RealTimeList from '../RealTimeList';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App tests', () => {
+  let component: Enzyme.ReactWrapper;
+  beforeEach(() => {
+    component = Enzyme.mount(
+      <Provider store={Store}>
+        <App />
+      </Provider>
+    );
+  })
+
+  it('Renders without crashing', () => {
+    expect(component.exists()).toBe(true);
+    expect(component.contains(<App />)).toBe(true);
+    expect(component.contains(<Header />)).toBe(true);
+    expect(component.contains(<RealTimeList />)).toBe(true);
+    const rl = component.find(RealTimeList);
+    expect(rl.props()).toEqual({ subscribeTo: undefined });
+  });
 });
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
-import immutableTransform from 'redux-persist-transform-immutable';
+import * as immutableTransform from 'redux-persist-transform-immutable';
 import { offline } from '@redux-offline/redux-offline';
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 import Items from './Items';
