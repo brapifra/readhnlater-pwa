1b70ef4bfa72b7c33c4b3e585d9978a65ee4bc7c
Brais Piñeiro <brapifra@gmail.com>
2018-09-02 10:12:18 +0100

    RealTimeList test

---
+++ src/containers/RealTimeList/RealTimeList.test.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import Store from '../../redux';
+import RealTimeList from '../RealTimeList';
+import ListItem from '../../components/ListItem';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('RealTimeList tests', () => {
+  it('Renders without crashing', () => {
+    const wrapper = Enzyme.shallow(<RealTimeList store={Store} />);
+    const wChild = wrapper.dive();
+    expect(wrapper.exists()).toBe(true);
+    expect(wChild.is(ListItem)).toBe(true);
+  });
+});
