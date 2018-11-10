dfbdc333100a1a5f9b28ce8a6a28ce9898522fe7
Brais Piñeiro <brapifra@gmail.com>
2018-11-10 17:51:22 +0000

    Collapsable comments

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -8,7 +8,7 @@ import ItemFetcher from '../ItemFetcher';
 const Container = styled.div`
   margin: 30px 0;
   font-size: 10pt;
-  margin-left: ${({ level }: { level: number }) => `${level * 40}px`};
+  margin-left: ${({ level }: { level: number }) => `${level * 20}px`};
   
   & a {
     :visited {
@@ -40,13 +40,30 @@ const Text = styled.div`
     margin: 10px 0 0 20px;
 `;
 
+const Collapse = styled.span`
+    cursor: pointer;
+
+    :hover{
+      text-decoration: underline;
+    }
+`;
+
 interface Props {
   level?: number;
 }
 
-export default class Comment extends React.PureComponent<ItemProperties & Props> {
+interface State {
+  collapsed: boolean;
+}
+
+export default class Comment extends React.PureComponent<ItemProperties & Props, State> {
+  public state: State = {
+    collapsed: false
+  }
+
   public render() {
     const { by, time, text, level, kids } = this.props;
+    const { collapsed } = this.state;
 
     const renderComment = (item: ItemProperties) => (
       <Comment {...item} level={(level || 0) + 1} />
@@ -62,12 +79,25 @@ export default class Comment extends React.PureComponent<ItemProperties & Props>
           <FaComment size={13} style={{ marginTop: 1 }} />
           <span>{by}</span>
           {time ? <span>{moment.unix(time).fromNow()}</span> : null}
+          <Collapse onClick={collapsed ? this.open : this.close}>
+            [{collapsed ? `+${(kids || []).length || ''}` : '-'}]
+          </Collapse>
         </Title>
-        <Text dangerouslySetInnerHTML={{ __html: text }} />
-        {(kids || []).map((kidId, index) => (
-          <ItemFetcher id={kidId.toString()} render={renderComment} key={index} />
-        ))}
+        {!collapsed ?
+          <div>
+            <Text dangerouslySetInnerHTML={{ __html: text }} />
+            {(kids || []).map((kidId, index) => (
+              <ItemFetcher id={kidId.toString()} render={renderComment} key={index} />
+            ))}
+          </div> :
+          null
+        }
       </Container>
     );
   }
+
+
+  private close = () => this.setState({ collapsed: true });
+
+  private open = () => this.setState({ collapsed: false });
 }
